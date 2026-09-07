5ebe26593fcb21d6da49fed3685038ccb1e381ab
wescleyplinio <wescleygaldino06@gmail.com>
2026-09-07 16:59:00 -0300

    Ajuste de responsividade do carrossel

--- portfolio/src/components/Icons.jsx
+++ portfolio/src/components/Icons.jsx
@@ -1,5 +1,3 @@
-import FadeIn from "./FadeIn";
-
 export default function Icons() {
     const techList = [
         { icon: "html5-plain", name: "HTML5" },
@@ -25,22 +23,20 @@ export default function Icons() {
     ];
 
     return (
-        <FadeIn>
-            <div className="grid grid-cols-3 sm:grid-cols-4 md:flex gap-3 md:gap-4 shrink-0 justify-items-center">
+        <div className="grid grid-cols-3 sm:grid-cols-4 md:flex gap-3 md:gap-4 shrink-0 justify-items-center">
+            
+            {techList.map((tech, index) => (
+                <div 
+                    key={index} 
+                    className="flex flex-col items-center justify-center p-3 md:p-5 border-2 border-blue-300 rounded-2xl md:rounded-4xl w-full md:w-auto hover:bg-blue-900/20 transition-colors duration-300 gap-2"
+                >
+                    <i className={`devicon-${tech.icon} text-white text-3xl sm:text-4xl md:text-6xl lg:text-8xl`}></i>
+                    <span className="text-white text-xs sm:text-sm font-medium tracking-wide">
+                        {tech.name}
+                    </span>
+                </div>
+            ))}
 
-                {techList.map((tech, index) => (
-                    <div
-                        key={index}
-                        className="flex flex-col items-center justify-center p-3 md:p-5 border-2 border-blue-300 rounded-2xl md:rounded-4xl w-full md:w-auto hover:bg-blue-900/20 transition-colors duration-300 gap-2"
-                    >
-                        <i className={`devicon-${tech.icon} text-white text-3xl sm:text-4xl md:text-6xl lg:text-8xl`}></i>
-                        <span className="text-white text-xs sm:text-sm font-medium tracking-wide">
-                            {tech.name}
-                        </span>
-                    </div>
-                ))}
-
-            </div>
-        </FadeIn>
+        </div>
     )
 }
--- portfolio/src/components/Section.jsx
+++ portfolio/src/components/Section.jsx
@@ -2,7 +2,7 @@ import FadeIn from "./FadeIn";
 
 export default function Section({ title, textOne, textTwo, children, id }) {
   return (
-    <section id={id} className="pt-20 mb-40">
+    <section id={id} className="pt-20 mb-50">
       <FadeIn>
         <h1 className="text-blue-300 shadow-blue-300 font-special-gothic text-3xl sm:text-7xl mb-10">
           {title}
